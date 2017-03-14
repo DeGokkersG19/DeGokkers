8d24b60f27734dc6b0aee4636f3742d6c9624eb8
Unknown <d233407@edu.rocwb.nl>
2017-03-14 14:40:47 +0100

    website

--- website/index.php
+++ website/index.php
@@ -9,97 +9,114 @@
     <title>Document</title>
 </head>
 <body>
-<div class="wrapper">
-    <div class="slideshow">
-        <img class="slide" src="img/andromeda-galaxy-755442_1280.jpg" alt="">
-        <img class="slide" src="img/milky-way-1655504_1280.jpg" alt="">
-        <img class="slide" src="img/milky-way-923801_1280.jpg" alt="">
-        <img class="slide" src="img/arch-896900_1280.jpg" alt="">
-    </div>
-    <div class="main-content">
-        <div class="container flex">
-            <header>
-                <div class="logo">
 
-                </div>
-                <nav class="navigation align-center">
-                    <ul>
-                        <li onclick="currentPage(1)">Home</li>
-                        <li onclick="currentPage(2)">About Me</li>
-                        <li onclick="currentPage(3)">Prices</li>
-                        <li onclick="currentPage(4)">Review</li>
-                        <li onclick="currentPage(5)">Contact</li>
-                    </ul>
-                </nav>
-            </header>
-            <div class="pages">
-                <div class="page">
-                    <h1>page1</h1>
-                </div>
-                <div class="page">
-                    <h1>page2</h1>
-                </div>
-                <div class="page">
-                    <h1>page3</h1>
-                </div>
-                <div class="page">
-                    <h1>Page4</h1>
-                </div>
-                <div class="page">
-                    <h1>Page5</h1>
+<div class="slideshow">
+    <img class="slide" src="img/andromeda-galaxy-755442_1280.jpg" alt="">
+    <img class="slide" src="img/milky-way-1655504_1280.jpg" alt="">
+    <img class="slide" src="img/milky-way-923801_1280.jpg" alt="">
+    <img class="slide" src="img/arch-896900_1280.jpg" alt="">
+</div>
+<div class="wrapper">
+    <div class="container">
+        <header class="flex-between">           
+            <div class="logo">
+                <h1>SpaceRace</h1>               
+            </div>
+            <nav class="navigation">
+                <ul class="flex-between"> 
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Gallery</li>
+                    <li>Contact</li>
+                </ul
+            </nav>
+        </header>
+        <div class="main-content">
+            <div class="home">
+                <h2>Welcome!</h2>
+                <div class="welcome-info flex-between">
+                    <div class="welcome-text">
+                        <p>Welcome to the official website of SpaceRace. here you can download a copy of the game for free to test it and give feedback to us.</p>
+                        <p>Here u have a sneak peak at the game en its UI.</p>
+                    </div>
+                    <img src="http://lorempixel.com/g/400/200/" alt="testuh">
                 </div>
             </div>
-            <footer>
-                <nav>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </nav>
-            </footer>
+            <div class="about">
+                <p>SpaceRace is a game that is based around horsebetting but with a special twist.
+                            the main purpose of the game is to have fun with your friends and maybe sometimes make some money of it
+                            within your friend group. the game is made by a group of developers that had an order to make a game
+                            that looks like horserace but in space with ufo's'. the extra task was to give a random spacehip a boost.</p>
+            </div>
+            <div class="gallery">
+            
+            </div>
+            <div class="contact">
+
+            </div>
         </div>
     </div>
-    <script>
-        var myIndex = 0;
-        slideShow();
 
-        function slideShow() {
-            var i;
-            var x = document.getElementsByClassName("slide");
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";
-            }
-            myIndex++;
-            if (myIndex > x.length) {myIndex = 1}
-            x[myIndex-1].style.display = "block";
-            setTimeout(slideShow, 5000);
-        }
-        var pageIndex = 1;
-        showPages(pageIndex);
 
-        function plusPage(n) {
-            showPages(pageIndex += n);
-        }
 
-        function currentPage(n) {
-            showPages(pageIndex = n);
-        }
 
-        function showPages(n) {
-            var i;
-            var x = document.getElementsByClassName("page");
-            var dots = document.getElementsByClassName("demo");
-            if (n > x.length) {pageIndex = 1}
-            if (n < 1) {pageIndex = x.length}
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace();
-            }
-            x[pageIndex-1].style.display = "block";
-            dots[pageIndex-1].className;
-        }
-    </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </div>
+<script>
+    var myIndex = 0;
+    slideShow();
+
+    function slideShow() {
+        var i;
+        var x = document.getElementsByClassName("slide");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}
+        x[myIndex-1].style.display = "block";
+        setTimeout(slideShow, 5000);
+    }
+    var pageIndex = 1;
+    showPages(pageIndex);
+
+    function plusPage(n) {
+        showPages(pageIndex += n);
+    }
+
+    function currentPage(n) {
+        showPages(pageIndex = n);
+    }
+
+    function showPages(n) {
+        var i;
+        var x = document.getElementsByClassName("page");
+        var dots = document.getElementsByClassName("demo");
+        if (n > x.length) {pageIndex = 1}
+        if (n < 1) {pageIndex = x.length}
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace();
+        }
+        x[pageIndex-1].style.display = "block";
+        dots[pageIndex-1].className;
+    }
+</script>
 </body>
 </html>
